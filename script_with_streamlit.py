be9770fa8709d0aae4f662a1b15b5e542cd36585
onath <onathatice97@gmail.com>
2024-02-18 13:26:13 +0100

    Dataset study, some plots, task and goals defined, streamlit initialization

---
+++ script_with_streamlit.py
@@ -0,0 +1,16 @@
+import streamlit as st
+
+
+ ###### HEADER ######
+st.title("FINAL PROJECT: STUDENTS EXAM SCORE")
+st.write ("Hello world")
+
+st.header("DATA EXPLORATION AND DATA WRANGLING")
+st.write ("first part of the project")
+
+st.subheader("EDA: Exploratory Data Analysis")
+
+st.subheader("PLOTS")
+
+st.header("MODEL")
+st.write("find a model that explains data")

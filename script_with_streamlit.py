e9b7940f7cd43d3e6d19993f5a5ed7cfd1feb27e
onath <onathatice97@gmail.com>
2024-02-22 19:30:45 +0100

    enhanced code cleanliness and streamlit presentation

--- script_with_streamlit.py
+++ script_with_streamlit.py
@@ -18,7 +18,7 @@ import function as fc
 def main():
 
     st.title("STUDENTS EXAM SCORE ANALYSIS")
-    st.write("## Introduction")
+    st.write("# Introduction")
     st.write('''The project aims to analyze a dataset containing lifestyle and exam score information about students. 
              Its primary objective is to predict students' scores using regression algorithms. 
              After experimenting with various algorithms, polynomial regression emerged as the most effective method.''')
@@ -28,22 +28,26 @@ def main():
     pd.options.display.float_format = '{:,.2f}'.format
     
     st.write("# Data Exploration")
-    #DATASET, RAW DATA
+
+#################################
+#DATASET, RAW DATA
+#################################
+    
     st.write("## Dataset Overview")
     st.write("Check out [dataset](https://www.kaggle.com/datasets/desalegngeb/students-exam-scores?select=Expanded_data_with_more_features.csv)")
     with st.expander("More info about Features"):
         features = [
-            "Gender: Gender of the student (male=0/female=1)",
-            "EthnicGroup: Ethnic group of the student (group A to E, one-hot encoding)",
+            "Gender: Gender of the student (male/female)",
+            "EthnicGroup: Ethnic group of the student (group A to E)",
             "ParentEduc: Parent(s) education background (from some_highschool to master's degree)",
-            "LunchType: School lunch type (standard=0 or free/reduced=1)",
-            "TestPrep: Test preparation course followed (completed=1 or none=0)",
-            "ParentMaritalStatus: Parent(s) marital status (married/single/widowed/divorced, one-hot encoding)",
-            "PracticeSport: How often the student practices sport (never=0/sometimes=1/regularly=2)",
-            "IsFirstChild: If the child is the first child in the family or not (yes=1/no=0)",
+            "LunchType: School lunch type (standard or free/reduced)",
+            "TestPrep: Test preparation course followed (completed or none)",
+            "ParentMaritalStatus: Parent(s) marital status (married/single/widowed/divorced)",
+            "PracticeSport: How often the student practices sport (never/sometimes/regularly)",
+            "IsFirstChild: If the child is the first child in the family or not (yes/no)",
             "NrSiblings: Number of siblings the student has (0 to 7)",
-            "TransportMeans: Means of transport to school (schoolbus=0/private=1)",
-            "WklyStudyHours: Weekly self-study hours (less than 5hrs =0; between 5 and 10hrs =1; more than 10hrs =2)",
+            "TransportMeans: Means of transport to school (schoolbus/private)",
+            "WklyStudyHours: Weekly self-study hours (less than 5hrs; between 5 and 10hrs; more than 10hrs)",
             "MathScore: math test score (0-100)",
             "ReadingScore: reading test score (0-100)",
             "WritingScore: writing test score (0-100)"
@@ -56,28 +60,31 @@ def main():
     num_cols = len(df_exam_score.select_dtypes(include='number').columns)
     fig, axes = plt.subplots(1, num_cols, figsize=(num_cols * 6, 6))
 
-    # Plot histograms for all numerical columns
+#################################
+# Plot histograms for all numerical columns
+#################################
     for i, column in enumerate(df_exam_score.select_dtypes(include='number').columns):
         df_exam_score[column].hist(ax=axes[i], bins=10)
         axes[i].set_title(f'Histogram of {column}')
         axes[i].set_xlabel(column)
         axes[i].set_ylabel('Frequency')
 
-    # Adjust layout and display
     plt.tight_layout()
     st.pyplot(fig)
     
-### DATASET CLEANING ###
-    st.write("# Dataset Cleaning and Handling missing values")
+#################################
+# DATASET CLEANING 
+#################################
+    st.write("## Dataset Cleaning and Handling missing values")
     df_exam_score.drop('Unnamed: 0',axis=1, inplace=True)
     
     # Cleaning parent education by replacing specific values with a more general value. 
     df_exam_score.loc[df_exam_score['ParentEduc'] == 'some high school', 'ParentEduc'] = 'high school'
     df_exam_score.loc[df_exam_score['ParentEduc'] == 'some college', 'ParentEduc'] = "bachelor's degree"
 
-   ####### NUMERICAL ENCODING FOR EACH VARIABLE #######
-    # ParentEduc: Numerical encoding from 0 to 3 for different education levels (0 is lower and 3 is higher) #######
+    # NUMERICAL ENCODING FOR EACH VARIABLE
     
+    # ParentEduc: Numerical encoding from 0 to 3 for different education levels (0 is lower and 3 is higher) #######
     replace_parentEduc={
             'high school':0,
             "bachelor's degree":1,
@@ -137,7 +144,10 @@ def main():
         }
     df_exam_score['WklyStudyHours'].replace(replace_WklyStudyHours, inplace=True)
 
-    # IMPUTING MISSING VALUES WITH RANDOM FOREST
+#################################
+# IMPUTING MISSING VALUES WITH RANDOM FOREST
+#################################
+    
     for column in df_exam_score.columns:
         if df_exam_score[column].isna().any():
             df_exam_score = fc.impute_missing_values_with_random_forest(df_exam_score, column)
@@ -148,24 +158,44 @@ def main():
     df_exam_score.drop('EthnicGroup_nan', axis=1, inplace=True)
     df_exam_score.drop('ParentMaritalStatus_nan', axis=1, inplace=True)
 
-    # st.dataframe(df_exam_score)
-    # fig, axes = plt.subplots(nrows=1, ncols=3, figsize=(12, 4))
-    # df_exam_score.plot(kind='hist', bins=100, ax=axes[0], title='Score')
+    st.write("Firstly I did a numerical encoding for  each variable, because in the original dataset features are indicated with categorical values, which is not useful to continue with analysis. ")
+    
+    with st.expander("Cleaned dataset features are as follows:"):
+        cleaned_dataset = [
+            "EthnicGroup and ParentMaritalStatus: I used a different approach, which is One-hot encoding: Each ethnic group has a different binary variable.",
+            "Gender: Binary encoding: 1 for female, 0 for male.",
+            "ParentEduc: Numerical encoding from 0 to 3 for different education levels (0 is lower and 3 is higher)",
+            "LunchType: Binary encoding: 0 for standard, 1 for free/reduced.",
+            "TestPrep: Test preparation course followed (completed=1 or none=0)",
+            "ParentMaritalStatus: Parent(s) marital status (married/single/widowed/divorced, one-hot encoding)",
+            "PracticeSport: Numerical encoding: 0 for never, 1 for sometimes, 2 for regularly.",
+            "IsFirstChild: Binary encoding: 1 for yes, 0 for no",
+            "NrSiblings: Number of siblings the student has (0 to 7)",
+            "TransportMeans: Binary encoding: 0 for schoolbus, 1 for private transport.",
+            "WklyStudyHours:** Numerical encoding: 0 for <5 hours, 1 for 5-10 hours, 2 for >10 hours.",
+            "MathScore: math test score (0-100)",
+            "ReadingScore: reading test score (0-100)",
+            "WritingScore: writing test score (0-100)"
+        ]
+        for cleaned_dataset in cleaned_dataset:
+            st.write(cleaned_dataset)
+    st.write("Then I impute missing values with Random Forest. Because of its efficiency and adaptability.")
 
-    # for ax in axes:
-    #     ax.set_xlabel('Score')
-    #     ax.set_ylabel('Frequency')
-
-    # plt.tight_layout() # to avoid overlap
-    # plt.show()
     st.write("Dataframe after cleaning.")
-    st.dataframe(df_exam_score)
+    st.dataframe(df_exam_score.head())
 
-    st.write("### Plot exam scores")
+#################################
+# PLOT    
+#################################
+
+    st.write("## Dataset Plot")
+    st.write("### Plot exam scores. Used two ways of visualization.")
     st.write('''The plot displays how students' scores are spread across different subjects, 
             revealing slight variations.Generally, students perform better in reading and writing, 
             as indicated by the predominance of scores above 35-40 to 100 in these subjects. 
             In the other hand, math scores show a wider range, with numerous scores falling also below 30.''')
+    
+    # First way
     fig = plt.figure(figsize=(12, 4))
 
     plt.subplot(1, 3, 1)
@@ -189,9 +219,7 @@ def main():
     plt.tight_layout()
     st.pyplot(fig)
 
-#_______________###############_______________
-
-    st.write("decide if keep or not")
+    # Second way
     fig, axes = plt.subplots(nrows=1, ncols=3, figsize=(12, 4))
     df_exam_score['MathScore'].plot(kind='hist', bins=100, ax=axes[0], title='Math Score')
     df_exam_score['ReadingScore'].plot(kind='hist', bins=100, ax=axes[1], title='Reading Score')
@@ -201,38 +229,34 @@ def main():
         ax.set_xlabel('Score')
         ax.set_ylabel('Frequency')
 
-    plt.tight_layout() # to avoid overlap
+    plt.tight_layout() 
     plt.show()
 
-    st.write("## Dataset Plot")
+    
     st.pyplot(fig)
 
-
-#_______________###############_______________
-
+#################################
+# CORRELATION
+#################################
 
     st.write("## Correlation")
     df_exam_score.WklyStudyHours.unique()
     df_exam_score_corr = df_exam_score.corr()
     st.dataframe(df_exam_score_corr)
 
-    st.write('''Correlation comment: the most influencing features for scores are:
-                    - gender
-                    - parent education 
-                    - lunchtype
-                    - test preparation
-                    - sport practicing
-                    - weekly study hours
-                    - ethnic group
-                We can also see that reading and writing scores are more correlated. 
-                So we can say that who is good at reading is also good in writing. ''')
+    st.write('''Correlation comment: the most influencing features for scores are: gender,
+            parent education, lunchtype, test preparation, sport practicing, weekly study hours, ethnic group.
+            We can also see that reading and writing scores are more correlated. 
+            So we can say that who is good at reading is also good in writing. ''')
 
     fig, ax = plt.subplots(figsize=(18, 14))
     sns.heatmap(df_exam_score_corr, annot=True, ax=ax)
     st.pyplot(fig)
 
-    ### PLOTS GROUPBY gender###
-    st.title('Correlation between Exam Scores and Gender')
+    st.write("Here a detailed plot of some of the most influencing features.")
+    
+    # Plot correlation between Exam Scores and Gender 
+    st.write('### Correlation between Exam Scores and Gender')
     st.write("By analyzing this plot we can see that male students are better in Mathematics, instead female sudents are better in Reading and Writing.")
     
     scores_for_each_gender = df_exam_score.groupby('Gender').mean()
@@ -247,8 +271,8 @@ def main():
     ax.set_xticklabels(['Male', 'Female'])
     st.pyplot(fig)
 
-    ### PLOTS GROUPBY practicing sport###
-    st.title('Correlation between Exam Scores and Practicing Sport')
+    # Plot correlation between Exam Scores and Practicing Sport
+    st.write('### Correlation between Exam Scores and Practicing Sport')
     st.write("By this plot we can see that practicing sport regulary influence better student score, especially reading and writing scores.")
 
     scores_correlation_with_ethnic_group = df_exam_score.groupby('PracticeSport').mean()
@@ -263,15 +287,15 @@ def main():
     ax.set_xticklabels(['Never', 'Sometimes', 'Regularly'])
     st.pyplot(fig)
 
-    ### PLOTS GROUPBY ethnic group###
-    st.title('Correlation between Exam Scores and Ethnic Group')
+    # Correlation between Exam Scores and Ethnic Group
+    st.write('### Correlation between Exam Scores and Ethnic Group')
     st.write('''By this plot we can comment:
                 Students belonging to the Group A have better scores then other groups in all subjects. 
              And they are good espacially in Mathematics. Instead other groups are better in Reading and Writing. ''')
 
     ethnic_group_list = list(df_exam_score[['EthnicGroup_group A', 'EthnicGroup_group B', 'EthnicGroup_group C', 'EthnicGroup_group D', 'EthnicGroup_group E']])
     scores_correlation_with_ethnic_groups = df_exam_score.groupby(ethnic_group_list).mean()
-    scores_correlation_with_ethnic_groups[["MathScore", "ReadingScore", "WritingScore"]]
+    # scores_correlation_with_ethnic_groups[["MathScore", "ReadingScore", "WritingScore"]]
 
     fig, ax =  plt.subplots()
     scores_correlation_with_ethnic_groups[["MathScore", "ReadingScore", "WritingScore"]].plot(kind='bar', ax=ax, rot=0)
@@ -281,19 +305,26 @@ def main():
     ax.legend(title='Subject',loc='upper right', prop={'size': 4} )
     ax.set_xticklabels(['Group A', 'Group B', 'Group C', 'Group D', 'Group E'])
     st.pyplot(fig)
-#_______________########_MODEL_########_______________
+
+#################################
+# MODEL
+#################################
 
     st.write("# Model")
     st.write('''The goal is to predict students' score. Initially, I explored a variety of regression algorithms, 
              including Linear Regression, Polynomial Regression, Random Forest Regression, and XGBoost Regressor. 
-             After experimentation, Polynomial Regression yielded the highest accuracy.''')
+             After experimentation, Polynomial Regression yielded the highest accuracy.
+             Since scores slightly drop with degree increase, degree of 2 is accepted sufficient.''')
     # preparation for model training and evaluation
     X = df_exam_score.drop(['MathScore','ReadingScore','WritingScore'], axis=1)
     y_math=df_exam_score['MathScore']
     y_read=df_exam_score['ReadingScore']
     y_write=df_exam_score['WritingScore']
 
-    ## Functions
+#################################
+# MODEL FUNCTIONS
+#################################
+    
     def model_evaluation(model, X_test, y_test, model_name):
         y_pred = model.predict(X_test)
 
@@ -319,53 +350,50 @@ def main():
         
         return model
 
-    # DIFFERENT MODELS TRYING AND CHOSING THAT ONE WITH BEST SCORE
-    # st.write("First trial: LinearRegressor on Math Score.")
+#################################
+# MODEL CHOSING
+#################################
+
+    # First trial: LinearRegressor on Math Score
     model=LinearRegression()
     model_math=model_train(X,y_math,model)
 
-    # st.write("Second and Third Trial: PolyRegressor of degree=2 and degree=3 on Math Score")
-    # st.write("Since scores slightly drop with degree increase, degree of 2 is accepted sufficient.")
-
-    # Create polynomial features
+    # Second and Third Trial: PolyRegressor of degree=2 and degree=3 on Math Score
+    # Create polynomial features, then reate and fit the polynomial regression model
     poly_features = PolynomialFeatures(degree=2, include_bias=False)
     X_poly = poly_features.fit_transform(X)
-
-    # Create and fit the polynomial regression model
     model = LinearRegression()
     model_math=model_train(X_poly,y_math,model)
 
-    # Create polynomial features
+    # Create polynomial features, then reate and fit the polynomial regression model
     poly_features = PolynomialFeatures(degree=3, include_bias=False)
     X_poly = poly_features.fit_transform(X)
-
-    # Create and fit the polynomial regression model
     model = LinearRegression()
     model_math=model_train(X_poly,y_math,model)
 
 
-    # st.write("Fourth Trial: Random Forest Regression on Math Score")
+    # Fourth Trial: Random Forest Regression on Math Score
     model=RandomForestRegressor(n_estimators=100)
     model_math=model_train(X,y_math,model)
 
-    # st.write("Fifth Trial: XGBoost")
+    # Fifth Trial: XGBoost
     model = XGBRegressor(learning_rate=0.05,max_depth=2,n_estimators= 150)
     model_math=model_train(X,y_read,model)
 
-    # Create a DataFrame to display the evaluation metrics
-    # Train and evaluate the math model
+    # Display the evaluation metrics
+    # Math model
     model.fit(X_poly, y_math)
     y_pred_math = model.predict(X_poly)
     mse_math = mean_squared_error(y_math, y_pred_math)
     r2_math = r2_score(y_math, y_pred_math)
 
-    # Train and evaluate the reading model
+    # Reading model
     model.fit(X_poly, y_read)
     y_pred_read = model.predict(X_poly)
     mse_read = mean_squared_error(y_read, y_pred_read)
     r2_read = r2_score(y_read, y_pred_read)
 
-    # Train and evaluate the writing model
+    # Writing model
     model.fit(X_poly, y_write)
     y_pred_write = model.predict(X_poly)
     mse_write = mean_squared_error(y_write, y_pred_write)
@@ -378,9 +406,9 @@ def main():
         'R2 Score': [r2_math, r2_read, r2_write]
     }
 
-    df = pd.DataFrame(data)
+    model_df = pd.DataFrame(data)
 
-    st.dataframe(df)
+    st.dataframe(model_df)
 
     
 
